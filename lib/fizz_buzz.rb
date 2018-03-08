fb9ad451f4dbd1c3d16bf6a44c88bf3a8fb0c9b6
aaing <n.y1031@gmail.com>
2018-03-08 10:56:19 +0900

    add some conditions and edit
    getの利用、行末にカンマを打たない

--- lib/fizz_buzz.rb
+++ lib/fizz_buzz.rb
@@ -1,19 +1,19 @@
-def fizz_buzz(n)
-  if n % 3 == 0 && n % 5 == 0
-    'FizzBuzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 == 0
-    'Buzz'
-  else
-    n.to_s
+def fizz_buzz
+  max = gets.to_i
+  str = ''
+  for n in 1..max do
+    if n % 3 == 0 && n % 5 == 0
+      str.concat('FizzBuzz')
+    elsif n % 3 == 0
+      str.concat('Fizz')
+    elsif n % 5 == 0
+      str.concat('Buzz')
+    else
+      str.concat(n.to_s)
+    end
+    str.concat(',') unless n == max
   end
+  puts str
 end
 
-puts fizz_buzz(1)
-puts fizz_buzz(2)
-puts fizz_buzz(3)
-puts fizz_buzz(4)
-puts fizz_buzz(5)
-puts fizz_buzz(6)
-puts fizz_buzz(15)
+fizz_buzz
